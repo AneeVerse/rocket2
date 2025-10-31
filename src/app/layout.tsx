54d2f5987a5e2187ff280beed8cf9b322da77793
Aneeverse <83137173+AneeVerse@users.noreply.github.com>
2025-10-31 11:55:36 +0530

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,20 @@ const exo = Exo({
   display: "swap",
 });
 
+
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K3442NKB7M"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K3442NKB7M');
+</script>
+
+
 export const metadata: Metadata = {
   title: "Name Change Gazette - Your Trusted Legal Partners",
   description: "Expert legal guidance and solutions tailored to your unique needs. Relax and leave legal worries to us.",
